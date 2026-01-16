3ce1fbc4a12bbc7198b05e982f90680cc15c275c
Vansh <vanshjainbro@gmail.com>
2026-01-16 16:01:35 +0530

    Day 1 - practice (Array Basics)

---
+++ src/ArrBasic.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+import java.util.*;
+public class ArrBasic {
+    public static void main(String[] args) {
+
+
+        int marks[] = new int[100];
+
+        Scanner sc = new Scanner(System.in);
+//        marks[0] = sc.nextInt();
+//        marks[1] = sc.nextInt();
+//        marks[2] = sc.nextInt();
+//
+//        System.out.println("Physics : "+marks[0]+" , Chemistry : "+marks[1]+" , Maths : "+marks[2]);
+        System.out.println(marks.length);
+
+
+    }
+}
