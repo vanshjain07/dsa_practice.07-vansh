483e1a406b4eefabf0c923fcd8d86d882357806b
Vansh <vanshjainbro@gmail.com>
2026-01-28 21:40:36 +0530

    Day 13 - Switch Case

---
+++ src/SwitchCase.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class SwitchCase {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        char operation = sc.next().charAt(0);
+        switch (operation){
+            case '+' :
+                System.out.println(a+b);
+                break;
+            case '-' :
+                System.out.println(a-b);
+                break;
+            case '*' :
+                System.out.println(a*b);
+                break;
+            case '/' :
+                System.out.println(a/b);
+                break;
+            default :
+                System.out.println("O goooodmorning pineapple");
+
+        }
+    }
+}
